696de479e7a5c0fd3d844b2a818e13dcf3c441f8
Mark Romel Feguro <121288515+Kaizn16@users.noreply.github.com>
2025-06-02 21:32:16 +0800

    added notification when not authenticated yet

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,15 +1,41 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import ToastNotification from "./ToastNotification";
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { isLoggedIn } = useAuth();
 
-  if (!isLoggedIn) {
-    return <Navigate to={"/"} replace />;
+  const [showToast, setShowToast] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      setShowToast(true);
+      const timer = setTimeout(() => {
+        setRedirect(true);
+      }, 1500);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isLoggedIn]);
+
+  if (!isLoggedIn && redirect) {
+    return <Navigate to="/" replace />;
   }
 
-  return children;
+  if (!isLoggedIn) {
+    return (
+      <ToastNotification
+        message="You must be logged in to access this page."
+        type="error"
+        onClose={() => setShowToast(false)}
+        isVisible={showToast}
+      />
+    );
+  }
+
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
